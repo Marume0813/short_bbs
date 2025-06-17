277b19a7c96d3b7df121e25871906df1ec0a122e
Marume0813 <2301204@s.asojuku.ac.jp>
2025-06-17 11:37:54 +0900

    formに修正

--- check.php
+++ check.php
@@ -11,7 +11,7 @@ $user = $stmt->fetch();
 
 if ($user && password_verify($password, $user['password'])) {
     $_SESSION['user'] = $user;
-    header("Location: index.php");
+    header("Location: form.php");
     exit;
 } else {
     echo "ログインに失敗しました";
