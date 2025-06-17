24b95a17615eacd2f097ec040691e39fd55ddd25
Marume0813 <2301204@s.asojuku.ac.jp>
2025-06-17 10:33:36 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
+<h1>ログイン</h1>
 <form action="check.php" method="post">
-    ユーザー名：<input type="text" name="username"><br>
-    パスワード：<input type="password" name="password"><br>
+    <p>ユーザー名：<input type="text" name="username"></p>
+    <p>パスワード：<input type="password" name="password"></p>
     <input type="submit" value="ログイン">
 </form>
+<p><a href="register.php">ユーザー登録</a></p>
